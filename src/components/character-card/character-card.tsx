03f2e465af4adf0a04cfa8628e401fbad5ecd819
Dmitrii Samsonenko <samsondm97@gmail.com>
2024-11-13 22:51:31 +0300

    feat: add interactive animation for cards

--- src/components/character-card/character-card.tsx
+++ src/components/character-card/character-card.tsx
@@ -1,14 +1,37 @@
+'use client';
 import TCharacter from '@/types/character';
 import Image from 'next/image';
 import styles from './character-card.module.scss';
-import { Suspense } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 import FlippingLoader from '../flipping-loader/flipping-loader';
 
 const CharacterCard = ({ character }: { character: TCharacter }) => {
+  const [isHover, setIsHover] = useState<boolean>(false);
+  const [imageRotation, setImageRotation] = useState<number>(0);
+  const [cardRotation, setCardRotation] = useState<number>(0);
+
+  const getHoverStyle = () => {
+    if (isHover) {
+      return { transform: `scale(1.1)` };
+    }
+    return { transform: `rotate(${cardRotation}deg)` };
+  };
+
+  useEffect(() => {
+    setCardRotation(Math.random() * 6 - 3);
+    setImageRotation(Math.random() * 6 - 3);
+  }, []);
+
   return (
     <div
       className={styles.card}
-      style={{ transform: `rotate(${Math.random() * 6 - 3}deg)` }}
+      style={getHoverStyle()}
+      onMouseEnter={() => {
+        setIsHover(true);
+      }}
+      onMouseLeave={() => {
+        setIsHover(false);
+      }}
     >
       <Suspense fallback={<FlippingLoader />}>
         <Image
@@ -16,7 +39,7 @@ const CharacterCard = ({ character }: { character: TCharacter }) => {
           alt={character.name.eng}
           width={150}
           height={150}
-          style={{ transform: `rotate(${Math.random() * 6 - 3}deg)` }}
+          style={{ transform: `rotate(${imageRotation}deg)` }}
         />
         <div className={styles['character-name']}>
           <p>{character.name.eng}</p>
