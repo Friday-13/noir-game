d46051ad15586b050aa8dbdb934d8f5dbb879938
Dmitrii Samsonenko <samsondm97@gmail.com>
2025-01-23 23:41:24 +0300

    feat: add auth endpoints

--- server/server/api/__init__.py
+++ server/server/api/__init__.py
@@ -1,1 +1,6 @@
-pass
+"""
+Module `api`
+
+Provides endpoints for the application, handling HTTP requests and responses.  
+Includes routes for data retrieval, creation, update, and deletion.
+"""
--- server/server/api/auth.py
+++ server/server/api/auth.py
@@ -1,19 +1,61 @@
 from typing import Annotated
-from fastapi import APIRouter, Body
 
+from fastapi import APIRouter, Body, HTTPException, Response
+from sqlalchemy import select
+
+from server.core.security import auth
+from server.db.models import UserModel
+from server.db.session import SessionDep
 from server.schemas.auth import UserLoginScheme, UserRegisterScheme
 
-auth_router = APIRouter(prefix='', tags=['auth'])
+auth_router = APIRouter(prefix="", tags=["auth"])
+
 
 @auth_router.post("/register")
-def register(user: Annotated[UserRegisterScheme, Body(embed=False)]):
-    print(user.username)
-    print(user.email)
-    return {'access_token': user.username}
+async def register(
+    user: Annotated[UserRegisterScheme, Body(embed=False)],
+    session: SessionDep,
+    response: Response,
+):
+    query = select(UserModel).where(
+        (UserModel.name == user.name) | (UserModel.email == user.email)
+    )
+    user_in_db = await session.execute(query)
+    existed_user = user_in_db.scalar_one_or_none()
+    if existed_user:
+        if existed_user.email == user.email:
+            raise HTTPException(401, {"message": "User with this email exists"})
+        if existed_user.name == user.name:
+            raise HTTPException(401, {"message": "User with this username exists"})
+    # TODO: add password hashing
+    new_user = UserModel(name=user.name, email=user.email, pass_hash=user.password)
+    session.add(new_user)
+    await session.flush()
+    token = auth.create_access_token(uid=str(new_user.id))
+    auth.set_access_cookies(response=response, token=token)
+    await session.commit()
+    return {"access_token": token}
+
 
 @auth_router.post("/login")
-def login(user: Annotated[UserLoginScheme, Body(embed=True)]):
-    print(user.email_or_username)
-    print(user.password)
-    return {'access_token': user.email_or_username}
+async def login(
+    user: Annotated[UserLoginScheme, Body(embed=True)],
+    session: SessionDep,
+    response: Response,
+):
+    query = select(UserModel).where(
+        (UserModel.email == user.email_or_name) | (UserModel.name == user.email_or_name)
+    )
+    data = await session.execute(query)
+    user_in_db = data.scalar_one_or_none()
+    if not user_in_db:
+        raise HTTPException(
+            401, detail={"message": "User with the provided username does not exist."}
+        )
+    # TODO: add passwod hashing
+    if user_in_db.pass_hash != user.password:
+        raise HTTPException(401, detail={"message": "Invalid credentials"})
 
+    token = auth.create_access_token(uid=str(user_in_db.id))
+    auth.set_access_cookies(response=response, token=token)
+    return {"access_token": token}
