549f0d3e51f0346866dbc9d11a9750d7ee8752ff
Dmitrii Samsonenko <samsondm97@gmail.com>
2025-02-14 00:07:02 +0300

    feat: add refresh token endpoint

--- server/server/api/auth.py
+++ server/server/api/auth.py
@@ -1,13 +1,15 @@
 from typing import Annotated
 
+from authx import RequestToken, TokenPayload
 from fastapi import APIRouter, Body, Depends, Response
 
-from server.core.security import auth, header_scheme
+from server.core.security import access_header_scheme, auth, refresh_header_scheme
 from server.db.models import UserRepository
 from server.db.session import SessionDep
 from server.schemas.auth import UserLoginScheme, UserRegisterScheme
 from server.utils.api import (
     set_user_auth,
+    update_user_auth,
     validate_password,
     validate_user_existance,
     validate_user_uniqueness,
@@ -29,8 +31,8 @@ async def register(
     new_user = await UserRepository.create_user(
         session, name=user.name, email=user.email, password=user.password
     )
-    token = set_user_auth(response, new_user.id)
-    return {"access_token": token}
+    token, refresh_token = set_user_auth(response, new_user.id)
+    return {"access_token": token, "refresh_token": refresh_token}
 
 
 @auth_router.post("/login")
@@ -44,13 +46,13 @@ async def login(
     )
     user_in_db = validate_user_existance(user_in_db)
     validate_password(user.password, user_in_db.pass_hash)
-    token = set_user_auth(response, user_in_db.id)
-    return {"access_token": token}
+    token, refresh_token = set_user_auth(response, user_in_db.id)
+    return {"access_token": token, "refresh_token": refresh_token}
 
 
 @auth_router.get(
     "/protected",
-    dependencies=[Depends(auth.access_token_required), Depends(header_scheme)],
+    dependencies=[Depends(auth.access_token_required), Depends(access_header_scheme)],
 )
 def protected():
     print("access granted")
@@ -59,7 +61,25 @@ def protected():
 
 @auth_router.get(
     "/is-auth",
-    dependencies=[Depends(auth.access_token_required), Depends(header_scheme)],
+    dependencies=[Depends(auth.access_token_required), Depends(access_header_scheme)],
 )
 def check_auth():
     return {"isAuth": True}
+
+
+@auth_router.post(
+    "/refresh",
+    dependencies=[
+        Depends(auth.refresh_token_required),
+        Depends(refresh_header_scheme),
+    ],
+)
+def refresh(
+    response: Response,
+    refresh_payload: TokenPayload = Depends(auth.refresh_token_required),
+    refresh_token_request: RequestToken = Depends(auth.get_refresh_token_from_request),
+):
+    token, refresh_token = update_user_auth(
+        response, refresh_payload, refresh_token_request
+    )
+    return {"access_token": token, "refresh_token": refresh_token}
--- server/server/utils/api.py
+++ server/server/utils/api.py
@@ -1,7 +1,10 @@
-from fastapi import HTTPException
+from datetime import timedelta
+
+from authx import RequestToken, TokenPayload
+from fastapi import HTTPException, Response
 from passlib.hash import bcrypt
 
-from server.core.security import auth
+from server.core.security import auth, config
 from server.db.models import UserModel
 from server.schemas.auth import UserRegisterScheme
 
@@ -30,7 +33,34 @@ def validate_password(password, pass_hash):
         raise HTTPException(401, detail={"message": "Invalid credentials"})
 
 
-def set_user_auth(response, uid):
+def set_access_token(response: Response, uid):
     token = auth.create_access_token(uid=str(uid))
     auth.set_access_cookies(response=response, token=token)
     return token
+
+
+def set_refresh_token(response: Response, uid):
+    max_age = 0
+    if isinstance(config.JWT_REFRESH_TOKEN_EXPIRES, timedelta):
+        max_age = int(config.JWT_REFRESH_TOKEN_EXPIRES.total_seconds())
+    refresh_token = auth.create_refresh_token(uid=str(uid))
+    auth.set_refresh_cookies(refresh_token, response, max_age)
+    return refresh_token
+
+
+def set_user_auth(response: Response, uid):
+    token = set_access_token(response, uid)
+    refresh_token = set_refresh_token(response, uid)
+    return token, refresh_token
+
+
+def update_user_auth(
+    response: Response,
+    refresh_payload: TokenPayload,
+    refresh_token_request: RequestToken,
+):
+    uid = refresh_payload.sub
+    if refresh_payload.time_until_expiry > config.JWT_IMPLICIT_REFRESH_DELTATIME:
+        token = set_access_token(response, uid)
+        return token, refresh_token_request.token
+    return set_user_auth(response, uid)
