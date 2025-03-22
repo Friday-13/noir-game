09ff092261d91aab42b7ee35d6d0366cdda54b3b
Dmitrii Samsonenko <samsondm97@gmail.com>
2025-03-23 01:16:09 +0300

    build: add test configs

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,33 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import path from "path";
+import tsconfig from "./tsconfig.app.json";
+
+const alias = Object.fromEntries(
+  Object.entries(tsconfig.compilerOptions.paths).map(([key, [value]]) => [
+    key.replace("/*", ""),
+    path.resolve(__dirname, value.replace("/*", "")),
+  ])
+);
 
 // https://vite.dev/config/
 export default defineConfig({
+  test: {
+    globals: true,
+    environment: "jsdom",
+    alias,
+    setupFiles: ["./src/tests/setup.ts", "./src/tests/__mocks__/store.ts"],
+    coverage: {
+      provider: "v8",
+      exclude: [
+        "./src/vite-env.d.ts",
+        "./eslint.config.js",
+        "./global.d.ts",
+        "./vite.config.ts",
+      ],
+    },
+  },
   server: {
     host: "127.0.0.1",
   },
@@ -22,7 +45,7 @@ export default defineConfig({
     preprocessorOptions: {
       scss: {
         quietDeps: true,
-      }
-    }
+      },
+    },
   },
 });
