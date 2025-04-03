57ff7969656669a8950299f59369ae38bb5446ec
Dmitrii Samsonenko <samsondm97@gmail.com>
2025-04-04 00:02:36 +0300

    test: add tests for login and register reducers

--- client/src/tests/__helpers__/get-test-auth-state.ts
+++ client/src/tests/__helpers__/get-test-auth-state.ts
@@ -1,13 +1,25 @@
-import { IAuthState } from "@/store/auth-slice";
+import {
+  IAuthState,
+  ICredentials,
+  ILogin,
+  IRegister,
+} from "@/store/auth-slice";
 
-export const getTestAuthState = (partialState: Partial<IAuthState>) => {
+export const getTestUserCrendentials = (
+  partialCredentials: Partial<ICredentials> = {},
+) => {
+  const user = {
+    name: "John Doe",
+    refresh_token: "test-refresh-token-123",
+    auth_token: "test-auth-token-456",
+    ...partialCredentials,
+  };
+  return user;
+};
+
+export const getTestAuthState = (partialState: Partial<IAuthState> = {}) => {
   const state: IAuthState = {
-    user: {
-      name: "John Doe",
-      refresh_token: "test-refresh-token-123",
-      auth_token: "test-auth-token-456",
-      ...partialState.user,
-    },
+    user: getTestUserCrendentials(partialState.user ?? {}),
     isAuth: partialState.isAuth ?? true,
     error: partialState.error ?? null,
   };
@@ -15,6 +27,25 @@ export const getTestAuthState = (partialState: Partial<IAuthState>) => {
   return state;
 };
 
-export const getTestUnauthorizedAuthState = (error?: string) => {
+export const getTestUnauthorizedAuthState = (error: string | null = null) => {
   return getTestAuthState({ user: null, isAuth: false, error: error });
+};
+
+export const getTestLoginData = (partialData: Partial<ILogin> = {}) => {
+  const data: ILogin = {
+    nameOrEmail: "John Doe",
+    password: "test-password-123",
+    ...partialData,
+  };
+  return data;
+};
+
+export const getTestRegisterData = (partialData: Partial<IRegister> = {}) => {
+  const data: IRegister = {
+    name: "John Doe",
+    email: "john.doe@example.com",
+    password: "test-password-123",
+    ...partialData,
+  };
+  return data;
 };
--- client/src/tests/store/auth-slice.test.tsx
+++ client/src/tests/store/auth-slice.test.tsx
@@ -1,11 +1,11 @@
 import { describe, expect, it, vi } from "vitest";
-import reducer, {
-  logout,
-  setAuth,
-} from "@/store/auth-slice";
+import reducer, { login, logout, register, setAuth } from "@/store/auth-slice";
 import {
   getTestAuthState,
+  getTestLoginData,
   getTestUnauthorizedAuthState,
+  getTestUserCrendentials,
+  getTestRegisterData,
 } from "@tests/__helpers__/get-test-auth-state";
 
 describe("Auth reducers test", async () => {
@@ -13,28 +13,60 @@ describe("Auth reducers test", async () => {
 
   it("Set new auth state", () => {
     const initialState = getTestUnauthorizedAuthState();
-    const finalState = getTestAuthState({});
+    const finalState = getTestAuthState();
 
     const resultState = reducer(initialState, setAuth(finalState));
     expect(resultState.isAuth).toBeTruthy();
   });
 
   it("Logout", () => {
-    const initialState = getTestAuthState({});
+    const initialState = getTestAuthState();
     const logoutState = reducer(initialState, logout());
 
     expect(logoutState.isAuth).not.toBeTruthy();
     expect(logoutState.user).toBeNull();
   });
 
-  // it("Success login", async () => {
-  //   const state = reducer(
-  //     undefined,
-  //     login.fulfilled({ name: "", refresh_token: "", auth_token: "" }, "", {
-  //       nameOrEmail: "",
-  //       password: "",
-  //     })
-  //   );
-  //   expect(state.isAuth).toBeTruthy();
-  // });
+  it("Success login", () => {
+    const userData = getTestLoginData();
+    const state = reducer(
+      undefined,
+      login.fulfilled(getTestUserCrendentials(), "", userData),
+    );
+    expect(state.isAuth).toBeTruthy();
+    expect(state.user?.name).toBe(userData.nameOrEmail);
+    expect(state.error).toBeNull();
+  });
+
+  it("Failed login", async () => {
+    const errorPayload = "Test error message";
+    const state = reducer(
+      undefined,
+      login.rejected(null, "", getTestLoginData(), errorPayload),
+    );
+    expect(state.isAuth).toBeFalsy();
+    expect(state.error).toBe(errorPayload);
+  });
+
+  it("Success registration", () => {
+    const userData = getTestRegisterData();
+    const state = reducer(
+      undefined,
+      register.fulfilled(getTestUserCrendentials(), "", userData),
+    );
+
+    expect(state.isAuth).toBeTruthy();
+    expect(state.error).toBeNull();
+    expect(state.user?.name).toBe(userData.name);
+  });
+
+  it("Failed registration", async () => {
+    const errorPayload = "Test error message";
+    const state = reducer(
+      undefined,
+      register.rejected(null, "", getTestRegisterData(), errorPayload),
+    );
+    expect(state.isAuth).toBeFalsy();
+    expect(state.error).toBe(errorPayload);
+  });
 });
