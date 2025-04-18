c17284a849fc1c3e9eacc4d0417818c4ea808a1e
Dmitrii Samsonenko <samsondm97@gmail.com>
2025-04-19 01:20:22 +0300

    refactor: separate connection logic and auth logic, add layer of authentication logic

--- server/server/api/game.py
+++ server/server/api/game.py
@@ -1,6 +1,6 @@
 import asyncio
 import json
-from typing import Optional
+from typing import Generic, TypeVar
 
 import pydantic
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect
@@ -14,31 +14,48 @@ from server.utils.auth_config import get_token_payload
 game_router = APIRouter(prefix="/games", tags=["game"])
 
 
+class BaseMessage(BaseModel):
+    type: str
+
+
+class AuthMessage(BaseMessage):
+    token: str
+
+
+class AuthException(Exception):
+    pass
+
+
 class Connection:
-    def __init__(self, websocket: WebSocket, user: Optional[UserModel] = None):
+    def __init__(self, websocket: WebSocket):
         self.websocket = websocket
+
+
+class AuthConnection(Connection):
+    def __init__(self, websocket: WebSocket, user: UserModel):
+        super().__init__(websocket)
         self.user = user
 
-    def set_user(self, user: Optional[UserModel]):
-        self.user = user
+
+T = TypeVar("T", bound=Connection)
 
 
-class ConnectionManager:
+class ConnectionManager(Generic[T]):
     def __init__(self):
-        self.active_clients: list[Connection] = []
+        self.active_clients: list[T] = []
 
-    async def connect(self, client: Connection):
+    async def connect(self, client: T):
         await client.websocket.accept()
         self.active_clients.append(client)
 
-    async def disconnect(self, client: Connection, code: int = 1000):
+    async def disconnect(self, client: T, code: int = 1000):
         await client.websocket.close(code)
         self.active_clients.remove(client)
 
-    async def send_personal_message(self, message: str, client: Connection):
+    async def send_personal_message(self, message: str, client: T):
         await client.websocket.send_text(message)
 
-    async def broadcast(self, message: str, from_client: Connection):
+    async def broadcast(self, message: str, from_client: T):
         for client in self.active_clients:
             if client != from_client:
                 await client.websocket.send_text(message)
@@ -48,8 +65,8 @@ class ConnectionManager:
             await client.websocket.send_text(message)
 
 
-class GameConnectionManager(ConnectionManager):
-    async def wait_for_introducing(self, client: Connection, timeout: float = 30.0):
+class AuthService:
+    async def handshake(self, client: Connection, timeout: float = 30.0):
         try:
             welcome_message = await asyncio.wait_for(
                 client.websocket.receive_text(), timeout=timeout
@@ -67,7 +84,7 @@ class GameConnectionManager(ConnectionManager):
             message = f"Validation error {e}"
             raise AuthException(message) from e
 
-    async def verify_user_by_token(self, session: SessionDep, token: str):
+    async def get_user_by_token(self, session: SessionDep, token: str):
         token_is_active = await TokenRepository.is_active(session, token)
         if not token_is_active:
             message = "Token is inactive"
@@ -80,16 +97,53 @@ class GameConnectionManager(ConnectionManager):
             raise AuthException(message)
         return user
 
-
-manager = GameConnectionManager()
-
-
-class BaseMessage(BaseModel):
-    type: str
+    def authenticate(self, conn: Connection, user: UserModel) -> AuthConnection:
+        return AuthConnection(conn.websocket, user)
 
 
-class AuthMessage(BaseMessage):
-    token: str
+class GameConnectionManager(ConnectionManager):
+    def __init__(self, authentication_service: AuthService):
+        super().__init__()
+        self.active_clients: list[AuthConnection] = []
+        self.auth_service = authentication_service
+
+    async def authorize_connection(
+        self, unauth_connection: Connection, session: SessionDep
+    ):
+        try:
+            token = await self.auth_service.handshake(unauth_connection)
+            user = await self.auth_service.get_user_by_token(session, token)
+            auth_connection = self.auth_service.authenticate(unauth_connection, user)
+            await self.connect(auth_connection)
+            return auth_connection
+        except AuthException as e:
+            await self.send_personal_message(str(e), unauth_connection)
+            if unauth_connection in self.active_clients:
+                await self.disconnect(unauth_connection)
+            else:
+                await unauth_connection.websocket.close()
+            return None
+
+
+auth_service = AuthService()
+manager = GameConnectionManager(auth_service)
+
+
+@game_router.websocket("/ws")
+async def game(session: SessionDep, websocket: WebSocket):
+    unauth_client = Connection(websocket)
+    client = await manager.authorize_connection(unauth_client, session)
+    if not client:
+        return
+
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.send_personal_message(f"You wrote: {data}", client)
+            await manager.broadcast(f"Client says: {data}", client)
+    except WebSocketDisconnect:
+        await manager.disconnect(client)
+        await manager.broadcast("Client left the chat", client)
 
 
 @game_router.post("/create")
@@ -100,32 +154,3 @@ async def create_game():
 @game_router.get("/list")
 async def list_game():
     pass
-
-
-class AuthException(Exception):
-    pass
-
-
-@game_router.websocket("/ws")
-async def game(session: SessionDep, websocket: WebSocket):
-    client = Connection(websocket)
-    await manager.connect(client)
-    try:
-        token = await manager.wait_for_introducing(client)
-        user = await manager.verify_user_by_token(session, token)
-        client.set_user(user)
-        if client.user:
-            await manager.broadcast_to_all(f"User {client.user.name} added to session")
-    except AuthException as e:
-        await manager.send_personal_message(str(e), client)
-        await manager.disconnect(client)
-        return None
-
-    try:
-        while True:
-            data = await websocket.receive_text()
-            await manager.send_personal_message(f"You wrote: {data}", client)
-            await manager.broadcast(f"Client says: {data}", client)
-    except WebSocketDisconnect:
-        await manager.disconnect(client)
-        await manager.broadcast("Client left the chat", client)
